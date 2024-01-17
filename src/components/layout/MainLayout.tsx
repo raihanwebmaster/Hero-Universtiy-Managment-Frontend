ce0b23969608a7fa02218967539efce697794872
raihanwebmaster <raihanemon2015@gmail.com>
2024-01-17 17:30:49 +0100

    setup sidebar and routes dynamic way

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,31 +1,9 @@
 /** @format */
-import { Layout, Menu, MenuProps } from "antd";
+import { Layout, Menu } from "antd";
+import { Outlet } from "react-router-dom";
+import { adminSidebarItems } from "../../routes/admin.routes";
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps["items"] = [
-  {
-    key: "1",
-    label: "Dashboard",
-  },
-  {
-    key: "2",
-    label: "Profile",
-  },
-  {
-    key: "3",
-    label: "User Management",
-    children: [
-      {
-        key: "11",
-        label: "Create Admin",
-      },
-      {
-        key: "22",
-        label: "Create Student",
-      },
-    ],
-  },
-];
 
 const MainLayout = () => {
   return (
@@ -55,7 +33,7 @@ const MainLayout = () => {
           theme='dark'
           mode='inline'
           defaultSelectedKeys={["4"]}
-          items={items}
+          items={adminSidebarItems}
         />
       </Sider>
       <Layout>
@@ -67,7 +45,7 @@ const MainLayout = () => {
               minHeight: 360,
             }}
           >
-            <h1>The main content should go here</h1>
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
---
+++ src/pages/admin/CreateStudent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateStudent = () => {
+  return (
+    <div>
+        <h1>This is CreateStudent component</h1>
+    </div>
+  )
+}
+
+export default CreateStudent
